ebea5bd8feacf106613f2da3cb25af887beaa5b8
jiiink <bhkim0404@gmail.com>
2025-10-16 14:45:16 +0900

    Solve: 1000 A+B

---
+++ solved_class/class1/1000_A+B.py
@@ -0,0 +1,24 @@
+# 1000_A+B.py
+# https://www.acmicpc.net/problem/1000
+# 알고리즘: 
+# 핵심 아이디어: 
+
+import sys
+
+# 빠른 입력 (input() 대신 사용)
+# input = sys.stdin.readline
+
+def main():
+    a, b = map(int, input().split())
+    print(a + b)
+
+
+def solve():
+    # 여기에 풀이 로직 작성
+    return
+
+
+if __name__ == "__main__":
+    # 테스트 입력이 있을 경우 아래 주석 해제
+    sys.stdin = open("input.txt", "r")
+    main()
