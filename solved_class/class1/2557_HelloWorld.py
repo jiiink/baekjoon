64a9563a909ad1a3f08ddba29c29c3458a1b306c
jiiink <bhkim0404@gmail.com>
2025-10-16 14:24:22 +0900

    Solve: 2557 HelloWorld

---
+++ solved_class/class1/2557_HelloWorld.py
@@ -0,0 +1,10 @@
+# 2557_HelloWorld.py
+# https://www.acmicpc.net/problem/2557
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    # 테스트 입력이 있을 경우 아래 주석 해제
+    # sys.stdin = open("input.txt", "r")
+    main()
