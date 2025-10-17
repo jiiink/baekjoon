96478f8fb674539903360eb1c93d785f31ba5f3c
jiiink <bhkim0404@gmail.com>
2025-10-17 09:43:08 +0900

    Solve: 1152_단어의 개수

---
+++ solved_class/class1/1152_단어의 개수.py
@@ -0,0 +1,24 @@
+# 1152_단어의 개수.py
+# https://www.acmicpc.net/problem/1152
+# 알고리즘: 
+# 핵심 아이디어: 
+
+import sys
+
+# 빠른 입력 (input() 대신 사용)
+input = sys.stdin.readline
+
+def main():
+
+    arr = list(input().split())
+
+    print(len(arr))
+
+def solve():
+    # 여기에 풀이 로직 작성
+    return
+
+if __name__ == "__main__":
+    # 테스트 입력이 있을 경우 아래 주석 해제
+    # sys.stdin = open("input.txt", "r")
+    main()
