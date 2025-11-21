c07aad1ef182c579a0865c2d42588de26b247e63
jiiink <bhkim0404@gmail.com>
2025-11-21 11:40:51 +0900

    Solve: 1931

--- solved_class/class3/1931_회의실 배정.py
+++ solved_class/class3/1931_회의실 배정.py
@@ -1,36 +1,41 @@
-# 문제번호_제목.py
-# url
+# 1931_회의실 배정.py
+# https://www.acmicpc.net/problem/1931
 # 알고리즘: 
-# 핵심 아이디어: 
+# 핵심 아이디어: 끝나는 시간 기준 오름차순 정렬
 
-# Windows 버전
-# Windows version 2
 import sys
 
 # 빠른 입력 (input() 대신 사용)
 input = sys.stdin.readline
 
 def main(): 
-    # 입력 받기 예시 -----------------------------
-    # 한 줄에 정수 하나
-    n = int(input().strip())
+
+    meetings = []
+
+    N = int(input())
  
-    # 한 줄에 여러 정수
-    arr = list(map(int, input().split()))
+    for _ in range(N):
+        start, end = map(int, input().split())
+        meetings.append((start, end))
 
-    # 문자열
-    s = input().strip()
+    # print(seminars)
 
-    # --------------------------------------------
+    meetings.sort(key=lambda x: (x[1], x[0]))
 
-    # 문제 풀이 로직 작성 -------------------------
-    result = sum(arr) + n
-    print(result)
-    # --------------------------------------------
+    # print(meetings)
 
-def solve():
-    # 여기에 풀이 로직 작성
-    return
+    accepted = []
+
+    j = 0
+    for i in range(len(meetings)):
+        if accepted == []:
+            accepted.append(meetings[i])
+        elif meetings[i][0] >= accepted[j][1]:
+            accepted.append(meetings[i])
+            j += 1
+
+    # print(accepted)
+    print(len(accepted))
 
 if __name__ == "__main__":
     # 테스트 입력이 있을 경우 아래 주석 해제
