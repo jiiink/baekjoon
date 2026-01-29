fd77deab3a7aa149caa5a1f88e0c598a1ced906a
jiiink <bhkim0404@gmail.com>
2026-01-29 09:11:27 +0900

    .

---
+++ solved_class/class3/11399_ATM.py
@@ -0,0 +1,33 @@
+# 11399_ATM.py
+# https://www.acmicpc.net/problem/11399
+# 알고리즘: 
+# 핵심 아이디어: 
+
+import sys
+
+# 빠른 입력 (input() 대신 사용)
+input = sys.stdin.readline
+
+def solve(people):
+    # 여기에 풀이 로직 작성
+    people.sort()
+    times = [people[0]]
+
+    for i in range(1, len(people)):
+        times.append(people[i] + times[i-1])
+
+    result = sum(times)
+
+    return result
+
+if __name__ == "__main__":
+    # 테스트 입력이 있을 경우 아래 주석 해제
+    # sys.stdin = open("input.txt", "r")
+    # 입력 받기 예시 -----------------------------
+    # 한 줄에 정수 하나
+    N = int(input().strip())
+ 
+    # 한 줄에 여러 정수
+    arr = list(map(int, input().split()))
+
+    print(solve(arr))
